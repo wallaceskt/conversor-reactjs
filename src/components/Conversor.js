cb80fade1864b8cbcfbbdc044260cae31784cd65
Wallace Oliveira <wallacesales@gmail.com>
2020-08-29 18:41:28 -0300

    Adicionei o componente Conversor ao projeto.

---
+++ src/components/Conversor.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import './Conversor.css';
+
+export default class Conversor extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            moedaA_valor: "",
+            moedaB_valor: 0,
+        }
+
+        // bind para passar o objeto
+        this.converter = this.converter.bind(this);
+    }
+
+    converter() {
+        let de_para = `${this.props.moedaA}_${this.props.moedaB}`;
+        let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=y&apiKey=4382e4c9d90f9f2a5584`;
+        
+        fetch(url)
+        .then(res => {
+            return res.json()
+        })
+        .then(json => {
+            let cotacao = json[de_para].val;
+            let moedaB_valor = (parseFloat(this.state.moedaA_valor) * cotacao).toFixed(2);
+            this.setState({moedaB_valor});
+        })
+    }
+
+    render() {
+        return (
+            <div className="conversor">
+                <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
+                <input type="text" onChange={(event)=>this.setState({moedaA_valor:event.target.value})}/>
+                <button onClick={this.converter}>Converter</button>
+                <h2>{this.state.moedaB_valor}</h2>
+            </div>
+        );
+    }
+}
